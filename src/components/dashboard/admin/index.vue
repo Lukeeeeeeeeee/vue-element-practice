c4779c117cf3c008cc5ab65f235979abd5f3c0ba
SamoZ <15176981781@163.com>
2018-04-27 16:38:42 +0800

    添加lineChart组件

--- src/components/dashboard/admin/index.vue
+++ src/components/dashboard/admin/index.vue
@@ -1,15 +1,33 @@
 <template>
     <div class="dashboard-admin-container">
         <panel-group></panel-group>
+        <el-row style="background-color: #fff; padding: 16px 16px 0; margin-bottom: 32px;">
+            <line-chart :chart-data="lineChartData"></line-chart>
+        </el-row>
     </div>
 </template>
 
 <script>
-import PanelGroup from './components/panelGroup';
+import PanelGroup from './components/panelGroup'
+import LineChart from './components/lineChart'
+
+const lineChartData = {
+    newVisitis: {
+        expectedData: [100, 120, 161, 134, 105, 160, 165],
+        actualData: [120, 82, 91, 154, 162, 140, 145]
+    }
+}
+
 export default {
     name: 'admin-dashboard',
     components: {
-        PanelGroup
+        PanelGroup,
+        LineChart
+    },
+    data() {
+        return {
+            lineChartData: lineChartData.newVisitis
+        }
     }
 }
 </script>
@@ -18,7 +36,7 @@ export default {
 .dashboard-admin-container {
     padding: 32px;
     background-color: rgb(240, 242, 245);
-    
+
 }
 </style>
 
