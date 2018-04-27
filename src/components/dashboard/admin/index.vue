70b1b2e0b57e96bf56da00eeb8fb1ea196314206
SamoZ <15176981781@163.com>
2018-04-27 13:06:01 +0800

    新增panelGroup页面, svgIcon新增className属性

--- src/components/dashboard/admin/index.vue
+++ src/components/dashboard/admin/index.vue
@@ -1,16 +1,24 @@
 <template>
-    <div>
-        admin
+    <div class="dashboard-admin-container">
+        <panel-group></panel-group>
     </div>
 </template>
 
 <script>
+import PanelGroup from './components/panelGroup';
 export default {
-    
+    name: 'admin-dashboard',
+    components: {
+        PanelGroup
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.dashboard-admin-container {
+    padding: 32px;
+    background-color: rgb(240, 242, 245);
+    
+}
 </style>
 
